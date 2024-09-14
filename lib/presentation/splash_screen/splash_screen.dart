be1c9eeae01ace4167dd6541206eff31fa3dfa48
Clip228 <artemkolos_03@mail.ru>
2024-09-14 16:37:21 +0700

    fix splash screen

--- lib/presentation/splash_screen/splash_screen.dart
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -21,72 +21,74 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Assets.images.background.image(
-              fit: BoxFit.scaleDown,
+      body: ColoredBox(
+        color: Colors.red,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Assets.images.background
+                  .image(fit: BoxFit.fitWidth, width: double.infinity),
             ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 40.w,
-                  vertical: 63.h,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    topRight: Radius.circular(30),
-                    topLeft: Radius.circular(30),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 40.w,
+                    vertical: 63.h,
                   ),
-                  color: context.colorScheme.onError,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      topLeft: Radius.circular(30),
+                    ),
+                    color: context.colorScheme.onError,
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        context.locale.taska,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 46.sp,
+                          fontFamily: FontFamily.pollerOne,
+                          color: context.colorScheme.primary,
+                        ),
+                      ),
+                      Gap(16.h),
+                      Text(
+                        context.locale.presonalTaskTracker,
+                        style: TextStyle(
+                          fontFamily: FontFamily.poppins,
+                          fontSize: 37.sp,
+                          fontWeight: FontWeight.w700,
+                          color: context.colorScheme.tertiary,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Gap(10.h),
+                      Text(
+                        context.locale.taskaSlogan,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 14.sp,
+                          fontFamily: FontFamily.poppins,
+                          color: context.colorScheme.surface,
+                        ),
+                      ),
+                      Gap(66.h),
+                      AppButton(
+                        onTap: () => context.pushRoute(const MainFlowRoute()),
+                        title: context.locale.continueText,
+                      ),
+                    ],
+                  ),
                 ),
-                child: Column(
-                  children: [
-                    Text(
-                      context.locale.taska,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 46.sp,
-                        fontFamily: FontFamily.pollerOne,
-                        color: context.colorScheme.primary,
-                      ),
-                    ),
-                    Gap(16.h),
-                    Text(
-                      context.locale.presonalTaskTracker,
-                      style: TextStyle(
-                        fontFamily: FontFamily.poppins,
-                        fontSize: 37.sp,
-                        fontWeight: FontWeight.w700,
-                        color: context.colorScheme.tertiary,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    Gap(10.h),
-                    Text(
-                      context.locale.taskaSlogan,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 14.sp,
-                        fontFamily: FontFamily.poppins,
-                        color: context.colorScheme.surface,
-                      ),
-                    ),
-                    Gap(66.h),
-                    AppButton(
-                      onTap: () => context.pushRoute(const MainFlowRoute()),
-                      title: context.locale.continueText,
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
